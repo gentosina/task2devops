9147fa7005682cc258faec49153efd02d3041b33
Yauheni Holik <yauheni.holik@qweeco.com>
2018-02-13 18:36:25 +0300

    add lb.sh

---
+++ lb.sh
@@ -0,0 +1,12 @@
+   echo -e "192.168.1.101 servertom1\n192.168.1.102 servertom2" >> /etc/hosts
+   yum install java-1.8.0-openjdk -y
+   yum install httpd -y
+   systemctl enable httpd
+   cp /vagrant/mod_jk.so /etc/httpd/modules/
+   cp /vagrant/workers.properties /etc/httpd/conf/
+   cp /vagrant/myconf.conf /etc/httpd/conf/
+   cd /etc/httpd/conf/
+   cat myconf.conf >> httpd.conf
+   firewall-cmd --zone=public --add-port=80/tcp --permanent
+   systemctl start httpd 
+   
